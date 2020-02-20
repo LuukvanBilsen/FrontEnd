b46dca05a4923046e3afb51dca118f2240b26ee3
Bilsen,Luuk L. van <luuk.vanbilsen1@gmail.com>
2020-02-20 15:40:36 +0100

    worked on UI

---
+++ Demo/Assets/Scripts/Click.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 clickPosition = -Vector3.one;
+            clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 5f));
+
+            Debug.Log(clickPosition);
+        }
+    }
+}
